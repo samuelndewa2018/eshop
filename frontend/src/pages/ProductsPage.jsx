8e620f438224f5218bff68577f49aab0d71e3a8f
Bramuels <samuelndewa2018@gmail.com>
2023-07-01 02:31:23 -0700

    sorting styling

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -8,7 +8,6 @@ import ProductCard from "../components/Route/ProductCard/ProductCard";
 import styles from "../styles/styles";
 import Meta from "../components/Meta";
 import { categoriesData } from "../static/data";
-import { Select, Option } from "@material-tailwind/react";
 
 const ProductsPage = () => {
   const [searchParams] = useSearchParams();
@@ -50,7 +49,22 @@ const ProductsPage = () => {
   };
 
   // Function to handle category selection
-  const handleCategoryChange = (e) => {
+  const handleCategoryChange = (value) => {
+    const selectedCategory = value;
+    setSelectedCategory(selectedCategory);
+
+    if (selectedCategory === "") {
+      setData(allProducts);
+    } else {
+      const filteredData =
+        allProducts &&
+        allProducts.filter((i) => i.category === selectedCategory);
+      setData(filteredData);
+    }
+  };
+
+  // Function to handle category selection
+  const handleCategoryChange2 = (e) => {
     const selectedCategory = e.target.value;
     setSelectedCategory(selectedCategory);
 
@@ -75,62 +89,119 @@ const ProductsPage = () => {
           <Header activeHeading={3} />
           <br />
           <br />
-          <div className={`${styles.section}`}>
-            {/* Sorting options */}
-            <div
-              className="flex justify-start
-             mb-4"
-            >
-              <label
-              // className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          <div className={`${styles.section} flex gap-[20px]`}>
+            {/* Category selection */}
+            <div className="hidden lg:block ml-2 mb-2 w-48 text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+              <button
+                onClick={(value) => handleCategoryChange((value = ""))}
+                type="button"
+                className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
               >
-                Sort By:
-              </label>
-              <Select
-                value={sortBy}
-                onChange={handleSortChange}
-                label="Select Version"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                <svg
+                  aria-hidden="true"
+                  className="w-4 h-4 mr-2 fill-current"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Sort By Category
+              </button>
+              {categoriesData &&
+                categoriesData.map((i) => (
+                  <button
+                    key={i.title}
+                    onClick={(value) => handleCategoryChange((value = i.title))}
+                    type="button"
+                    className="text-start w-full px-4 py-2 text-sm border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
+                  >
+                    {i.title}
+                  </button>
+                ))}
+              <button
+                onClick={(value) => handleCategoryChange((value = ""))}
+                className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
+                type="button"
               >
-                {/* <select
-                
-               
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              > */}
-                <Option value="">Select</Option>
-                <Option value="priceLowToHigh">Price: Low to High</Option>
-                <Option value="priceHighToLow">Price: High to Low</Option>
-                <Option value="ratingHighToLow">Rating: High to Low</Option>
-                <Option value="salesHighToLow">Sales: High to Low</Option>
-              </Select>
+                Reset All
+              </button>
             </div>
 
-            {/* Category selection */}
-            <div
-              className="flex justify-start
-             mb-4"
-            >
-              <label className="mr-2">Category:</label>
-              <select value={selectedCategory} onChange={handleCategoryChange}>
-                <option value="Choose a category">Choose a category</option>
-                <option value="">All</option>
-                {categoriesData &&
-                  categoriesData.map((i) => (
-                    <option value={i.title} key={i.title}>
-                      {i.title}
-                    </option>
-                  ))}
-              </select>
+            <div className={`${styles.section}`}>
+              <div className="flex gap-2">
+                {/* Sorting options */}
+                <div className="lg:flex sm:block">
+                  <div>
+                    <label
+                      for="small"
+                      className="flex mt-2 mr-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Sort By:
+                    </label>
+                  </div>
+                  <div>
+                    <select
+                      value={sortBy}
+                      onChange={handleSortChange}
+                      id="small"
+                      className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >
+                      <option value="">Select</option>
+                      <option value="priceLowToHigh">Price: Low to High</option>
+                      <option value="priceHighToLow">Price: High to Low</option>
+                      <option value="ratingHighToLow">
+                        Rating: High to Low
+                      </option>
+                      <option value="salesHighToLow">Sales: High to Low</option>
+                    </select>
+                  </div>
+                </div>
+                {/* Sorting by category 2 */}
+                <div className="lg:flex sm:block">
+                  <div>
+                    <label
+                      for="small"
+                      className="flex mt-2 mr-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Sort By Category:
+                    </label>
+                  </div>
+                  <div>
+                    <select
+                      value={selectedCategory}
+                      onChange={handleCategoryChange2}
+                      id="small"
+                      className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >
+                      <option value="Choose a category">
+                        Choose a category
+                      </option>
+                      <option value="">All</option>
+                      {categoriesData &&
+                        categoriesData.map((i) => (
+                          <option value={i.title} key={i.title}>
+                            {i.title}
+                          </option>
+                        ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+                {data &&
+                  data.map((i, index) => <ProductCard data={i} key={index} />)}
+              </div>
+              {data && data.length === 0 ? (
+                <h1 className="text-center w-full pb-[100px] text-[20px]">
+                  No products Found!
+                </h1>
+              ) : null}
             </div>
-            <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-              {data &&
-                data.map((i, index) => <ProductCard data={i} key={index} />)}
-            </div>
-            {data && data.length === 0 ? (
-              <h1 className="text-center w-full pb-[100px] text-[20px]">
-                No products Found!
-              </h1>
-            ) : null}
           </div>
           <Footer />
         </div>
